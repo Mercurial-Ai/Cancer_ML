b7616fd9782852cd8dfcb9e1daa77b2841585353
Tpool1 <tristenpool89@gmail.com>
2021-07-04 14:57:15 -0500

    change target var

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ if useDefaults:
     test_file = ""
 
     # list with strings or a single string may be inputted
-    target_variables = 'Definitive Surgery Type'
+    target_variables = 'Field Strength (Tesla)'
 
     # if true, converted images will be in png format instead of jpg
     png = False
