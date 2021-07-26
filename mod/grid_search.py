e9eaf8c71a838500c477dc36d83a4ae06dbfcd6f
Tristen Pool <tristenpool89@gmail.com>
2021-07-26 01:30:08 -0600

    add write to excel functionality

--- mod/grid_search.py
+++ mod/grid_search.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from itertools import product
 import math
+import xlwt
+from xlwt import Workbook
 
 class grid_search:
     def __init__(self, grid_path):
@@ -34,3 +36,42 @@ class grid_search:
                 comb_dict_list.append(comb_dict)
 
         return comb_dict_list
+
+class write_excel:
+    def __init__(self, sheet_path, hyp_dict_list):
+        self.path = sheet_path
+        self.hyps = hyp_dict_list
+
+    def run(self):
+        wb = Workbook()
+
+        sheet = wb.add_sheet('Sheet 1')
+
+        # add column labels to sheet
+        i = 0
+        perf_tuple = self.hyps[0]
+        hyp_dict = perf_tuple[0]
+        labels = list(hyp_dict.keys())
+        for label in labels:
+            sheet.write(0, i, label)
+
+            i = i + 1
+
+        sheet.write(0, i, 'accuracy')
+
+        # add corresponding hyperparameters and performance data to each column
+        i = 1
+        for perf_tuple in self.hyps:
+            hyp_dict = perf_tuple[0]
+            accuracy = perf_tuple[-1]
+            j = 0
+            for label in labels:
+                sheet.write(i, j, hyp_dict[label])
+
+                j = j + 1
+
+            sheet.write(i, j, accuracy)
+
+            i = i + 1
+
+        wb.save(self.path)
