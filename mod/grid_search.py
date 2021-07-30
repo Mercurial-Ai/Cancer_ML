e7b81dcde7dee724a05ae0af6a4cc67e81452835
Tristen Pool <tristenpool89@gmail.com>
2021-07-30 15:51:37 -0500

    add randomized grid search

--- mod/grid_search.py
+++ mod/grid_search.py
@@ -1,9 +1,6 @@
-from os import terminal_size
 import pandas as pd
 from itertools import product
-import math
-import xlwt
-from xlwt import Workbook
+from random import shuffle
 
 class grid_search:
     def __init__(self, grid_path):
@@ -34,47 +31,6 @@ class grid_search:
 
                 comb_dict_list.append(comb_dict)
 
+        shuffle(comb_dict_list)
+
         return comb_dict_list
-
-class write_excel:
-    def __init__(self, sheet_path, hyp_dict_list):
-        self.path = sheet_path
-        self.hyps = hyp_dict_list
-
-    def run(self):
-        wb = Workbook()
-
-        sheet = wb.add_sheet('Sheet 1')
-
-        # add column labels to sheet
-        i = 0
-        perf_tuple = self.hyps[0]
-        hyp_dict = perf_tuple[0]
-        labels = list(hyp_dict.keys())
-        for label in labels:
-            sheet.write(0, i, label)
-
-            i = i + 1
-
-        sheet.write(0, i, 'accuracy')
-        sheet.write(0, i+1, 'iou score')
-
-        # add corresponding hyperparameters and performance data to each column
-        i = 1
-        for perf_tuple in self.hyps:
-            hyp_dict = perf_tuple[0]
-            accuracy_iou = perf_tuple[-1]
-            accuracy = accuracy_iou[0]
-            iou_score = accuracy_iou[1]
-            j = 0
-            for label in labels:
-                sheet.write(i, j, hyp_dict[label])
-
-                j = j + 1
-
-            sheet.write(i, j, accuracy)
-            sheet.write(i, j+1, iou_score)
-
-            i = i + 1
-
-        wb.save(self.path)
