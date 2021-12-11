75bf048458788b1a41753f49b64b078c70f70325
Tristen Pool <tristenpool89@gmail.com>
2021-12-10 21:50:11 -0600

    change to random forest classifier

--- src/isolation_forest.py
+++ src/isolation_forest.py
@@ -1,11 +1,12 @@
-from sklearn.ensemble import IsolationForest
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd 
 
 def isolation_forest(features, target):
 
-    isolated_forest=IsolationForest(n_estimators=100, n_jobs=-1,random_state=42) 
+    isolated_forest=RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=42, max_depth=6) 
 
-    predicted = isolated_forest.fit_predict(features, target)
+    isolated_forest.fit(features, target)
+    predicted = isolated_forest.predict(features)
 
     predicted_df = pd.DataFrame(predicted)
     predicted_df.to_csv('data_anomaly.csv')
