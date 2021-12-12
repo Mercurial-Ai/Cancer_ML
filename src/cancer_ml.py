067a875b24527c50506e020ad36236c36aa78eca
Tristen Pool <tristenpool89@gmail.com>
2021-12-12 16:10:17 -0600

    ensemble fixes

---
+++ src/cancer_ml.py
@@ -0,0 +1,486 @@
+from src.PeakCluster import PeakCluster
+from src.data_pipeline import data_pipeline
+from src.clinical_only import clinical_only
+from src.image_model import image_model
+from src.cnn import cnn
+from collections import Counter
+import math
+import pickle
+from src.isolation_forest import isolation_forest
+
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from src.random_crop import random_crop
+
+class cancer_ml:
+
+    def __init__(self, dataset, target, model="clinical_only", crop_size=(256, 256)):
+        self.dataset = dataset
+        self.target = target
+        self.crop_size=crop_size
+
+        if self.dataset == "duke":
+            self.collect_duke()
+        elif self.dataset == "hn1":
+            self.collect_HN1()
+        elif self.dataset == "metabric":
+            self.collect_METABRIC()
+
+        # initialize model bools
+        self.clinical = False
+        self.image_clinical = False
+        self.cnn = False
+
+        if model == "clinical_only":
+            self.clinical = True
+            self.data_pipe.only_clinical.X_train, self.data_pipe.only_clinical.y_train = self.remove_outliers(self.data_pipe.only_clinical.X_train, self.data_pipe.only_clinical.y_train)
+        elif model == "image_clinical":
+            self.image_clinical = True
+            self.data_pipe.image_clinical.X_train, self.data_pipe.image_clinical.y_train = self.remove_outliers(self.data_pipe.image_clinical.X_train, self.data_pipe.image_clinical.y_train)
+        elif model == "cnn":
+            self.cnn = True
+
+            pre_shape = self.data_pipe.image_only.X_train.shape
+            flattened_img_x = np.empty(shape=(pre_shape[0], pre_shape[1]*pre_shape[2]), dtype=np.int8)
+
+            i = 0
+            for img in self.data_pipe.image_only.X_train:
+                flattened_img = img.flatten()
+                flattened_img_x[i] = flattened_img
+
+                i = i + 1
+
+            flattened_img_x, self.data_pipe.image_only.y_train = self.remove_outliers(flattened_img_x, self.data_pipe.image_only.y_train)
+
+            x = np.reshape(flattened_img_x, pre_shape)
+            self.data_pipe.image_only.X_train = x
+
+            self.setup_cluster()
+            self.k_neighbors()
+
+    def collect_duke(self):
+        self.data_pipe = data_pipeline("data/Duke-Breast-Cancer-MRI/Clinical and Other Features (edited).csv", "data/Duke-Breast-Cancer-MRI/img_array_duke.npy", self.target)
+        self.data_pipe.load_data()
+    
+    def collect_HN1(self):
+        self.data_pipe = data_pipeline("data/HNSCC-HN1/Copy of HEAD-NECK-RADIOMICS-HN1 Clinical data updated July 2020 (adjusted chemotherapy var).csv", "data/HNSCC-HN1/img_array.npy", self.target)
+        self.data_pipe.load_data()
+
+    def collect_METABRIC(self):
+        self.data_pipe = data_pipeline("data/METABRIC_RNA_Mutation/METABRIC_RNA_Mutation.csv", None, self.target)
+        self.data_pipe.load_data()
+
+    def run_model(self):
+
+        if self.clinical:
+            self.model = clinical_only(load_model=False)
+            self.model.get_model(self.data_pipe.only_clinical.X_train, self.data_pipe.only_clinical.y_train, self.data_pipe.only_clinical.X_val, self.data_pipe.only_clinical.y_val)
+        elif self.image_clinical:
+            self.model = image_model(load_model=False)
+            self.model.get_model(self.data_pipe.image_clinical.X_train, self.data_pipe.image_clinical.y_train, self.data_pipe.image_clinical.X_val, self.data_pipe.image_clinical.y_val)
+        elif self.cnn:
+            self.model = cnn(load_model=False)
+            self.model.get_model(self.data_pipe.image_only.X_train, self.data_pipe.image_only.y_train, self.data_pipe.image_only.X_val, self.data_pipe.image_only.y_val)
+
+    def test_model(self):
+        
+        if self.clinical:
+            print(self.model.test_model(self.data_pipe.only_clinical.X_test, self.data_pipe.only_clinical.y_test))
+        elif self.image_clinical:
+            print(self.model.test_model(self.data_pipe.image_clinical.X_test, self.data_pipe.image_clinical.y_test))
+        elif self.cnn:
+            print(self.model.test_model(self.data_pipe.image_only.X_test, self.data_pipe.image_only.y_test))
+
+    def remove_outliers(self, X, y):
+        predicted = isolation_forest(X, y)
+        
+        non_outlier_indices = []
+        i = 0
+        for prediction in predicted:
+            if prediction != -1:
+                non_outlier_indices.append(i)
+
+            i = i + 1
+
+        num_outliers = len(predicted) - len(non_outlier_indices)
+
+        print("Num Outliers:", num_outliers)
+
+        X = X[non_outlier_indices, :]
+        if str(type(y)) == "<class 'numpy.ndarray'>":
+            y = y[non_outlier_indices]
+        else:
+            y = y.iloc[non_outlier_indices]
+
+        return X, y
+
+    def setup_cluster(self):
+        X = self.data_pipe.image_only.X_train
+
+        X = random_crop(X, (self.crop_size[0], self.crop_size[1], 1))   
+
+        X = np.reshape(X, (X.shape[0], X.shape[1]*X.shape[2]))
+        self.model = PeakCluster(X)
+
+        # determine number of each label
+        self.label_counts = dict(Counter(self.model.labels_))
+
+    def make_class_inference(self, image_array):
+
+        image_array = image_array.flatten()
+        image_array = np.expand_dims(image_array, axis=0)
+
+        inference = self.neigh.predict(image_array)
+
+        return inference
+
+    def k_neighbors(self):
+
+        X = self.data_pipe.image_only.X_train
+        X_test = self.data_pipe.image_only.X_test
+        X_val = self.data_pipe.image_only.X_val
+
+        X = random_crop(X, (self.crop_size[0], self.crop_size[1], 1))
+        X_test = random_crop(X_test, (self.crop_size[0], self.crop_size[1], 1))
+        X_val = random_crop(X_val, (self.crop_size[0], self.crop_size[1], 1))
+
+        self.data_pipe.image_only.X_test = X_test
+        self.data_pipe.image_only.X_val = X_val
+
+        # flatten X for KNeighbors
+        X = np.reshape(X, (X.shape[0], X.shape[1]*X.shape[2])).astype('float32')
+
+        n_clusters = len(list(set(self.model.labels_)))
+
+        self.neigh = KNeighborsClassifier(n_neighbors=n_clusters)
+        self.neigh.fit(X, self.model.labels_)
+
+        # unflatten X
+        X = np.reshape(X, (-1, int(math.sqrt(X.shape[1])), int(math.sqrt(X.shape[1]))))
+
+        self.data_pipe.image_only.X_train = self.equalize_classes(X)
+        self.data_pipe.image_only.X_test = self.equalize_test(X_test)
+        self.data_pipe.image_only.X_val = self.equalize_val(X_val)
+
+        self.equalize_image_clinical()
+
+    def get_classes(self):
+        return self.model.labels_
+
+    def equalize_image_clinical(self):
+        self.data_pipe.image_clinical.X_train = self.data_pipe.image_clinical.X_train[self.collected_indices_train]
+        self.data_pipe.image_clinical.X_test = self.data_pipe.image_clinical.X_test[self.collected_indices_test]
+        self.data_pipe.image_clinical.X_val = self.data_pipe.image_clinical.X_val[self.collected_indices_val]
+
+        self.data_pipe.image_clinical.y_train = self.data_pipe.image_clinical.y_train[self.collected_indices_train]
+        self.data_pipe.image_clinical.y_test = self.data_pipe.image_clinical.y_test[self.collected_indices_test]
+        self.data_pipe.image_clinical.y_val = self.data_pipe.image_clinical.y_val[self.collected_indices_val]
+
+    def equalize_test(self, img_array):
+
+        y_test = self.data_pipe.image_only.y_test
+
+        if str(type(y_test)) == "<class 'pandas.core.series.Series'>":
+            y_test = y_test.to_numpy()
+
+        # dictionary containing image data per label
+        class_array_dict = {}
+
+        # dictionary containing y data per label
+        class_y_dict = {}
+
+        inferences = []
+        for image in img_array:
+            inference = self.make_class_inference(image)[0]
+            inferences.append(inference)
+
+        label_counts = dict(Counter(inferences))
+        
+        num_clusters_used = 0
+        for label in list(label_counts.keys()):
+
+            count = label_counts[label]
+
+            if count != 1:
+                # list of indices utilized to collect y
+                collected_indices = []
+
+                class_array = np.empty(shape=(label_counts[label], img_array.shape[1], img_array.shape[2]), dtype=np.int8)
+            
+                i = 0
+                j = 0
+                for image in img_array:
+
+                    inference = inferences[i]
+
+                    if inference == label:
+                        image = np.squeeze(image)
+                        class_array[j] = image
+                        collected_indices.append(i)
+                        j = j + 1
+
+                    i = i + 1
+                        
+                class_array_dict[label] = class_array 
+
+                y = y_test[collected_indices]
+
+                class_y_dict[label] = y
+
+                num_clusters_used = num_clusters_used + 1
+
+        self.collected_indices_test = collected_indices
+
+        # find lowest count of labels excluding labels with only 1 instance
+        labels = list(label_counts.keys())
+
+        filtered_labels = []
+        for label in labels:
+            if self.label_counts[label] != 1:
+                filtered_labels.append(label_counts[label])
+        
+        lowest_count = min(filtered_labels)
+
+        for label in list(class_array_dict.keys()):
+
+            class_array = class_array_dict[label]
+            y_array = class_y_dict[label]
+
+            new_array = class_array[:lowest_count]
+            new_y = y_array[:lowest_count]
+
+            class_array_dict[label] = new_array
+            class_y_dict[label] = new_y
+
+        new_y = np.array([])
+        for array in list(class_y_dict.values()):
+            new_y = np.append(new_y, array)
+
+        self.data_pipe.image_only.y_test = new_y.astype('int8')
+
+        new_data = np.empty(shape=(lowest_count*num_clusters_used, self.crop_size[0], self.crop_size[1]), dtype=np.int8)
+        i = 0
+        for image_array in list(class_array_dict.values()):
+
+            for image in image_array:
+                new_data[i] = image
+
+                i = i + 1
+
+        new_data = np.expand_dims(new_data, axis=-1)
+
+        return new_data
+
+    def equalize_val(self, img_array):
+
+        y_val = self.data_pipe.image_only.y_val
+
+        if str(type(y_val)) == "<class 'pandas.core.series.Series'>":
+            y_val = y_val.to_numpy()
+
+        # dictionary containing image data per label
+        class_array_dict = {}
+
+        # dictionary containing y data per label
+        class_y_dict = {}
+
+        inferences = []
+        for image in img_array:
+            inference = self.make_class_inference(image)[0]
+            inferences.append(inference)
+
+        label_counts = dict(Counter(inferences))
+        
+        num_clusters_used = 0
+        for label in list(label_counts.keys()):
+
+            count = label_counts[label]
+
+            if count != 1:
+                # list of indices utilized to collect y
+                collected_indices = []
+
+                class_array = np.empty(shape=(label_counts[label], img_array.shape[1], img_array.shape[2]), dtype=np.int8)
+            
+                i = 0
+                j = 0
+                for image in img_array:
+
+                    inference = inferences[i]
+
+                    if inference == label:
+                        image = np.squeeze(image)
+                        class_array[j] = image
+                        collected_indices.append(i)
+                        j = j + 1
+
+                    i = i + 1
+                        
+                class_array_dict[label] = class_array 
+
+                y = y_val[collected_indices]
+
+                class_y_dict[label] = y
+
+                num_clusters_used = num_clusters_used + 1
+
+        self.collected_indices_val = collected_indices
+
+        # find lowest count of labels excluding labels with only 1 instance
+        labels = list(label_counts.keys())
+
+        filtered_labels = []
+        for label in labels:
+            if self.label_counts[label] != 1:
+                filtered_labels.append(label_counts[label])
+        
+        lowest_count = min(filtered_labels)
+
+        for label in list(class_array_dict.keys()):
+
+            class_array = class_array_dict[label]
+            y_array = class_y_dict[label]
+
+            new_array = class_array[:lowest_count]
+            new_y = y_array[:lowest_count]
+
+            class_array_dict[label] = new_array
+            class_y_dict[label] = new_y
+
+        new_y = np.array([])
+        for array in list(class_y_dict.values()):
+            new_y = np.append(new_y, array)
+
+        self.data_pipe.image_only.y_val = new_y.astype('int8')
+
+        new_data = np.empty(shape=(lowest_count*num_clusters_used, self.crop_size[0], self.crop_size[1]), dtype=np.int8)
+        i = 0
+        for image_array in list(class_array_dict.values()):
+
+            for image in image_array:
+                new_data[i] = image
+
+                i = i + 1
+
+        new_data = np.expand_dims(new_data, axis=-1)
+
+        return new_data
+
+    def divide_into_classes(self, image_array, n_clusters):
+
+        y_train = self.data_pipe.image_only.y_train
+
+        if str(type(y_train)) == "<class 'pandas.core.series.Series'>":
+            y_train = y_train.to_numpy()
+
+        # dictionary containing image data per label
+        class_array_dict = {}
+
+        # dictionary containing y data per label
+        class_y_dict = {}
+
+        self.num_clusters_used = 0
+        for label in range(n_clusters):
+
+            if self.label_counts[label] != 1:
+
+                # list of indices utilized to collect y
+                collected_indices = []
+
+                class_array = np.empty(shape=(self.label_counts[label], image_array.shape[1], image_array.shape[2]), dtype=np.int8)
+            
+                i = 0
+                j = 0
+                for image in image_array:
+
+                    inference = self.model.labels_[i]
+
+                    if inference == label:
+                        class_array[j] = image
+                        collected_indices.append(i)
+                        j = j + 1
+
+                    i = i + 1
+                        
+                class_array_dict[label] = class_array
+
+                y = y_train[collected_indices]
+
+                class_y_dict[label] = y
+
+                self.num_clusters_used = self.num_clusters_used + 1
+
+        self.collected_indices_train = collected_indices
+
+        return class_array_dict, class_y_dict
+
+    def equalize_classes(self, image_array):
+
+        # find lowest count of labels excluding labels with only 1 instance
+        labels = list(self.label_counts.keys())
+
+        filtered_labels = []
+        for label in labels:
+            if self.label_counts[label] != 1:
+                filtered_labels.append(self.label_counts[label])
+        
+        lowest_count = min(filtered_labels)
+
+        n_clusters = len(self.label_counts)
+        class_array_dict, class_y_dict = self.divide_into_classes(image_array, n_clusters)
+
+        for label in list(class_array_dict.keys()):
+
+            class_array = class_array_dict[label]
+            y_array = class_y_dict[label]
+
+            new_array = class_array[:lowest_count]
+            new_y = y_array[:lowest_count]
+
+            class_array_dict[label] = new_array
+            class_y_dict[label] = new_y
+
+        new_y = np.array([])
+        for array in list(class_y_dict.values()):
+            new_y = np.append(new_y, array)
+
+        self.data_pipe.image_only.y_train = new_y.astype('int8')
+
+        new_data = np.empty(shape=(lowest_count*self.num_clusters_used, self.crop_size[0], self.crop_size[1]), dtype=np.int8)
+        i = 0
+        for image_array in list(class_array_dict.values()):
+
+            for image in image_array:
+                new_data[i] = image
+
+                i = i + 1
+
+        new_data = np.expand_dims(new_data, axis=-1)
+
+        return new_data
+
+    def save_arrays(self):
+        imageFile = open('image_only.pickle', 'w+b')
+        pickle.dump(self.data_pipe.image_only, imageFile, protocol=4)
+        imageFile.close()
+
+        ICfile = open('image_clinical.pickle', 'w+b')
+        pickle.dump(self.data_pipe.image_clinical, ICfile, protocol=4)
+        ICfile.close()
+
+        clinicalFile = open('clinical_only.pickle', 'w+b')
+        pickle.dump(self.data_pipe.only_clinical, clinicalFile, protocol=4)
+        clinicalFile.close()
+
+    def load_arrays(self):
+        imageFile = open('image_only.pickle', 'r+b')
+        self.data_pipe.image_only = pickle.load(imageFile)
+        imageFile.close()
+
+        ICfile = open('image_clinical.pickle', 'r+b')
+        self.data_pipe.image_clinical = pickle.load(ICfile)
+        ICfile.close()
+
+        clinicalFile = open('clinical_only.pickle', 'r+b')
+        self.data_pipe.only_clinical = pickle.load(clinicalFile)
+        clinicalFile.close()
--- src/ensemble.py
+++ src/ensemble.py
@@ -4,40 +4,60 @@ from keras.models import load_model
 from scipy.sparse import data
 from sklearn.metrics import accuracy_score
 import numpy as np
-
-class data_pod:
-    def __init__(self, models, data):
-        self.models = models
-        self.data = data
+from src.cancer_ml import cancer_ml
 
 class voting_ensemble:
 
     # input iterables containing [x, y] for each data form
-    def __init__(self, clinical_test, image_clinical_test, image_only_test):
-        self.clinical_test = clinical_test
-        self.image_clinical_test = image_clinical_test
-        self.image_only_test = image_only_test
+    def __init__(self):
+        clinical = cancer_ml("metabric", "chemotherapy", model="clinical_only")
+        image_clinical = cancer_ml("duke", "Adjuvant Chemotherapy", model="image_clinical")
+        image_only = cancer_ml("duke", "Adjuvant Chemotherapy", model="cnn")
 
         self.clinical_models = self.load_models('data\\saved_models\\clinical')
         self.image_clinical_models = self.load_models('data\\saved_models\\image_clinical')
         self.image_only_models = self.load_models('data\\saved_models\\image_only')
 
-        clinical_only = data_pod(self.clinical_models, self.clinical_test)
-        image_clincial = data_pod(self.image_clinical_models, image_clinical_test)
-        image_only = data_pod(self.image_only_models, self.image_only_test)
+        all_models = [self.clinical_models, self.image_clinical_models, self.image_only_models]
 
-        all_models = [clinical_only, image_clincial, image_only]
+        clinical_test = [clinical.data_pipe.only_clinical.X_test, clinical.data_pipe.only_clinical.y_test]
+        image_clinical_test = [image_clinical.data_pipe.image_clinical.X_test, image_clinical.data_pipe.image_clinical.y_test]
+        image_only_test = [image_only.data_pipe.image_only.X_test, image_only.data_pipe.image_only.y_test]
 
-        for pod in all_models:
-            testX = pod.data[0]
-            testY = pod.data[1]
-            models = pod.models 
-           
-            try:
-                print('Ensemble Prediction:', self.predict(testX, models))
-                print('Ensemble Eval:', self.evaluate_models(testX, testY, models))
-            except IndexError:
-                print("Warning: empty model list")
+        all_data = [clinical_test, image_clinical_test, image_only_test]
+
+        i = 0
+        predictions = []
+        evals = []
+        for models in all_models:
+
+            # filter out empty model lists
+            if len(models) >= 1:
+                data = all_data[i]
+
+                testX = data[0]
+                testY = data[1]
+
+                ensemble_prediction = self.predict(testX, models)
+                predictions.append(ensemble_prediction)
+
+                ensemble_eval = self.evaluate_models(testX, testY, models)
+                evals.append(ensemble_eval)
+
+            i = i + 1
+
+        ensembled_predictions = []
+        ensembled_evals = []
+            
+        # find average of predictions and evals for ensembled values
+        for prediction in predictions:
+            ensembled_predictions.append(float(np.mean(prediction)))
+
+        for eval in evals:
+            ensembled_evals.append(float(np.mean(eval)))
+
+        self.ensembled_prediction = sum(ensembled_predictions) / len(ensembled_predictions)
+        self.ensembled_eval = sum(ensembled_evals) / len(ensembled_evals)
         
     def load_models(self, model_dir):
 
