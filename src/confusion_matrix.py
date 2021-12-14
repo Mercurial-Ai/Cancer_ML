bcb49940fb9c366e3f29cc14a01b74761cdfca12
Tristen Pool <tristenpool89@gmail.com>
2021-12-14 11:17:31 -0600

    confusion matrix fixes

--- src/confusion_matrix.py
+++ src/confusion_matrix.py
@@ -1,8 +1,9 @@
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix as c_mat 
+from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
 def confusion_matrix(y_true, y_pred):
-    c_matrix = confusion_matrix(y_true, y_pred)
+    c_matrix = c_mat(y_true, y_pred.round())
     disp = ConfusionMatrixDisplay(c_matrix)
 
     disp.plot()
