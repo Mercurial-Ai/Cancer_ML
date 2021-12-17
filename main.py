6818d3780dda56adea93425273fdbfab29acff92
Tristen Pool <tristenpool89@gmail.com>
2021-12-16 18:47:44 -0600

    set load_models to false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.ensemble import voting_ensemble
 
-en = voting_ensemble(load_models=True)
+en = voting_ensemble(load_models=False)
 
 print(en.ensembled_prediction)
 print(en.ensembled_eval)
