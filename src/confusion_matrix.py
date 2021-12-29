3eaee21a5195ee67037e4eaf28adf755fe9671bb
Tristen Pool <tristenpool89@gmail.com>
2021-12-28 18:53:30 -0600

    adjust save path for confusion matrices

--- src/confusion_matrix.py
+++ src/confusion_matrix.py
@@ -8,8 +8,13 @@ def confusion_matrix(y_true, y_pred):
 
     if type(y_true) == pd.DataFrame:
         vars = list(y_true.columns)
-        print(vars)
         y_true = y_true.to_numpy()
+    else:
+        # if y true is not dataframe, output vars are duke since it is from image only model
+        vars = ['Surgery', 'Days to Surgery (from the date of diagnosis)', 'Definitive Surgery Type', 'Clinical Response, Evaluated Through Imaging ', 'Pathologic Response to Neoadjuvant Therapy', 'Days to local recurrence (from the date of diagnosis) ', 'Days to distant recurrence(from the date of diagnosis) ', 'Days to death (from the date of diagnosis) ', 
+                            'Days to last local recurrence free assessment (from the date of diagnosis) ', 'Days to last distant recurrence free assemssment(from the date of diagnosis) ', 'Neoadjuvant Chemotherapy', 'Adjuvant Chemotherapy', 'Neoadjuvant Endocrine Therapy Medications ',
+                            'Adjuvant Endocrine Therapy Medications ', 'Therapeutic or Prophylactic Oophorectomy as part of Endocrine Therapy ', 'Neoadjuvant Anti-Her2 Neu Therapy', 'Adjuvant Anti-Her2 Neu Therapy ', 'Received Neoadjuvant Therapy or Not', 'Pathologic response to Neoadjuvant therapy: Pathologic stage (T) following neoadjuvant therapy ',
+                            'Pathologic response to Neoadjuvant therapy:  Pathologic stage (N) following neoadjuvant therapy', 'Pathologic response to Neoadjuvant therapy:  Pathologic stage (M) following neoadjuvant therapy ', 'Overall Near-complete Response:  Stricter Definition', 'Overall Near-complete Response:  Looser Definition', 'Near-complete Response (Graded Measure)']
 
     if len(y_true.shape) == 1:
         c_matrix = c_mat(y_true, y_pred.round())
@@ -24,13 +29,9 @@ def confusion_matrix(y_true, y_pred):
 
     else:
         y_pred = np.reshape(y_pred, (y_pred.shape[1], y_pred.shape[0]))
-        print(y_true.shape)
         for i in range(y_true.shape[1]):
             col1 = y_true[:, i]
             col2 = y_pred[:, i]
-
-            print(col1.shape)
-            print(col2.shape)
 
             c_matrix = c_mat(col1, col2.round())
             disp = ConfusionMatrixDisplay(c_matrix)
@@ -40,4 +41,4 @@ def confusion_matrix(y_true, y_pred):
             
             disp.plot()
 
-            plt.savefig("confusion_matrix " + vars[i])
+            plt.savefig("confusion_matrices/confusion_matrix " + vars[i])
