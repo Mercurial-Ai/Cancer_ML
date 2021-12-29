56b397db3068262f09d3f75cb2b1c5158c1b6a04
Tristen Pool <tristenpool89@gmail.com>
2021-12-28 18:20:25 -0600

    confusion matrix fixes

--- src/confusion_matrix.py
+++ src/confusion_matrix.py
@@ -7,6 +7,8 @@ import pandas as pd
 def confusion_matrix(y_true, y_pred):
 
     if type(y_true) == pd.DataFrame:
+        vars = list(y_true.columns)
+        print(vars)
         y_true = y_true.to_numpy()
 
     if len(y_true.shape) == 1:
@@ -22,18 +24,20 @@ def confusion_matrix(y_true, y_pred):
 
     else:
         y_pred = np.reshape(y_pred, (y_pred.shape[1], y_pred.shape[0]))
+        print(y_true.shape)
         for i in range(y_true.shape[1]):
-            for j in range(y_pred.shape[1]):
-                col1 = y_true[:, i]
-                col2 = y_pred[:, j]
+            col1 = y_true[:, i]
+            col2 = y_pred[:, i]
 
-                c_matrix = c_mat(col1, col2.round())
-                disp = ConfusionMatrixDisplay(c_matrix)
+            print(col1.shape)
+            print(col2.shape)
+
+            c_matrix = c_mat(col1, col2.round())
+            disp = ConfusionMatrixDisplay(c_matrix)
 
             # clear plt before plotting to prevent other graphs from appearing with .show()
             plt.close('all')
             
             disp.plot()
 
-            plt.savefig("confusion_matrix")
-            plt.show()
+            plt.savefig("confusion_matrix " + vars[i])
