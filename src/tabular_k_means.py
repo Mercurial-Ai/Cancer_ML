a19b81027b63f27ff38a074330232deeabb6101d
Tristen Pool <tristenpool89@gmail.com>
2022-03-02 11:16:13 -0600

    equalize classes in tabular k means

--- src/tabular_k_means.py
+++ src/tabular_k_means.py
@@ -1,11 +1,13 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
+from get_distribution import get_distribution
 from tokenize_dataset import tokenize_dataset
 from PeakCluster import PeakCluster
 from collections import Counter
 
-df = pd.read_csv("data\METABRIC_RNA_Mutation\METABRIC_RNA_Mutation.csv", low_memory=False)
+df = pd.read_csv("data\Duke-Breast-Cancer-MRI\Clinical and Other Features (edited).csv", low_memory=False)
 
 column_means = df.mean()
 df = df.fillna(column_means)
@@ -22,3 +24,26 @@ n_clusters = len(list(set(model.labels_)))
 
 neigh = KNeighborsClassifier(n_neighbors=n_clusters)
 neigh.fit(data, model.labels_)
+
+inferences = list(neigh.predict(data))
+inference_distribution = get_distribution(inferences)
+
+# equalize classes identified
+classes_equal = False
+indices_removed = []
+while not classes_equal:
+    most_frequent_class = max(set(inferences), key=inferences.count)
+    most_frequent_count = max(inference_distribution.values())*len(inferences)*0.01
+    least_frequent_count = min(inference_distribution.values())*len(inferences)*0.01
+    num_to_remove = int(round((most_frequent_count - least_frequent_count), 0))
+
+    for i in range(num_to_remove):
+        indices_removed.append(inferences.index(most_frequent_class))
+        data = np.delete(data, inferences.index(most_frequent_class), axis=0)
+        inferences.remove(most_frequent_class)
+
+    inference_distribution = get_distribution(inferences)
+
+    # if the distribution of all classes are equal
+    if all(element == list(inference_distribution.values())[0] for element in list(inference_distribution.values())):
+        classes_equal = True
