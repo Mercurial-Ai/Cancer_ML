f573cd1a87e8ebc905df9f3c67c0ce0a2ba2557c
Tristen Pool <tristenpool89@gmail.com>
2022-04-09 13:36:51 -0500

    image clinical train method

--- src/image_model.py
+++ src/image_model.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 from tensorflow import keras
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense
 from tensorflow.keras.layers import concatenate
@@ -10,6 +11,7 @@ from tensorflow.keras.metrics import AUC
 import torch
 import torch.nn as nn
 from src.resnet import resnet18
+import numpy as np
 
 class image_clinical(nn.Module):
     def __init__(self):
@@ -24,9 +26,11 @@ class image_clinical(nn.Module):
         self.fc3 = nn.Linear(25, 15)
 
     def image_track(self):
-        self.res = resnet18(pretrained=True)
+        self.res = resnet18(pretrained=False)
 
-    def forward(self, clinical_data, image_data):
+    def forward(self, data):
+        clinical_data = data[0]
+        image_data = data[1]
 
         # clinical
         clinical_x = self.relu(self.fc1(clinical_data))
@@ -36,7 +40,7 @@ class image_clinical(nn.Module):
         # image
         image_x = self.res(image_data)
 
-        x = torch.cat([clinical_x, image_x])
+        x = torch.cat([clinical_x, image_x], dim=1)
         self.fc1_cat = nn.Linear(x.shape[-1], 26)
         x = self.relu(self.fc1_cat(x))
         self.fc2_cat = nn.Linear(26, 1)
@@ -58,29 +62,27 @@ class image_model:
 
         self.model = image_clinical()
 
-        self.criterion = torch.nn.CrossEntropyLoss()
+        self.criterion = torch.nn.MSELoss()
         optimizer = torch.optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
 
         for epoch in range(epochs):
             running_loss = 0.0
-            for i in range((X_train.shape[0]-1)//batch_size + 1):
+            for i in range((X_train[0][1].shape[0]-1)//batch_size + 1):
                 start_i = i*batch_size
                 end_i = start_i+batch_size
 
-                xb = X_train[start_i:end_i]
-                yb = y_train[start_i:end_i]
+                xb = [torch.from_numpy(X_train[0][0][start_i:end_i]), torch.from_numpy(X_train[0][1][start_i:end_i])]
+                yb = torch.from_numpy(y_train[start_i:end_i])
 
-                xb_shape = xb.shape
+                xb_image_shape = xb[1].shape
 
-                xb = torch.from_numpy(xb)
-                yb = torch.from_numpy(yb)
-
-                xb = torch.reshape(xb, (xb_shape[0], 1, 256, 256))
-                xb = xb.type(torch.float)
+                xb[1] = torch.reshape(xb[1], (xb_image_shape[0], 1, 256, 256))
+                xb[1] = xb[1].type(torch.float)
+                xb[0] = xb[0].type(torch.float)
                 pred = self.model(xb)
-
                 pred = pred.to(torch.float)
-                yb = yb.to(torch.long)
+
+                yb = torch.reshape(yb, (yb.shape[0], 1)).type(torch.float)
                 loss = self.criterion(pred, yb)
         
                 loss.backward()
@@ -102,12 +104,15 @@ class image_model:
         return self.model
 
     def test_model(self, X_test, y_test):
+        X_test[0][1] = np.reshape(X_test[0][1], (X_test[0][1].shape[0], 1, 256, 256))
+        X_test = [torch.from_numpy(X_test[0][0]).type(torch.float), torch.from_numpy(X_test[0][1]).type(torch.float)]
+        y_test = torch.from_numpy(y_test).type(torch.float)
+        with torch.no_grad():
+            self.model.eval()
+            y_pred = self.model(X_test)
+            test_loss = self.criterion(y_pred, y_test)
 
-        results = self.model.evaluate(X_test, y_test, batch_size=32)
-
-        confusion_matrix(y_true=y_test, y_pred=self.model.predict(X_test), save_name="image_clinical_c_mat.png")
-
-        return results
+        return test_loss
 
     def get_model(self, X_train=None, y_train=None, X_val=None, y_val=None, epochs=10, batch_size=128):
         
