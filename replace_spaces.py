4af6024afd86a90f2b0401fcd74cc0f0d5cc1bfc
Tristen Pool <tristenpool89@gmail.com>
2022-05-13 23:21:00 -0500

    add replace spaces script

---
+++ replace_spaces.py
@@ -0,0 +1,10 @@
+import os
+base = os.getcwd()
+for patient in os.listdir("."):
+    patient = os.path.join(base, patient)
+    for dir in os.listdir(patient):
+        dir = os.path.join(base, dir)
+        for f in os.listdir(dir):
+            r = f.replace(" ", "_")
+            if(r != f):
+                os.rename(f,r)
