4cacb7aea92e5b20674f19a055d4bf9c68006475
Tristen Pool <tristenpool89@gmail.com>
2022-05-13 23:30:04 -0500

    fix replace spaces method

--- replace_spaces.py
+++ replace_spaces.py
@@ -3,7 +3,7 @@ base = os.getcwd()
 for patient in os.listdir("."):
     patient = os.path.join(base, patient)
     for dir in os.listdir(patient):
-        dir = os.path.join(base, dir)
+        dir = os.path.join(patient, dir)
         for f in os.listdir(dir):
             r = f.replace(" ", "_")
             if(r != f):
