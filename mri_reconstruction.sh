048e7570216d541dcfff9fc1f15fab2f1039402e
Tristen Pool <tristenpool89@gmail.com>
2022-05-13 23:19:26 -0500

    update mri reconstruction batch file

--- mri_reconstruction.sh
+++ mri_reconstruction.sh
@@ -1,4 +1,9 @@
-dir = "Duke-Breast-Cancer-MRI"
-for d in */ ; do
-    echo "$d"
+readarray -d '' paths < <(find . -maxdepth 3 -mindepth 3 -type d)
+for path in ${paths[@]}
+do
+    base=$(echo "$path" | cut -d "/" -f2)
+    save="mesh.stl"
+    save_path="$base$save"
+    echo $save_path
+    dicom2mesh -i $path -t 557 -o $save_path
 done
