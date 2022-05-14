085c70a3cde5f0773ab9b0b0e0e14dc8b35660b9
Tristen Pool <tristenpool89@gmail.com>
2022-05-14 08:52:11 -0500

    batch file formatting

--- mri_reconstruction.sh
+++ mri_reconstruction.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=28
+#SBATCH --nodes=1
+#SBATCH --partition=All
+#SBATCH -J rf_insert
+##SBATCH --time=12:00:00
+#SBATCH -e ./%J.err
+#SBATCH -o ./%J.out
+
+# execute program
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+hostname
+date
 readarray -d '' paths < <(find . -maxdepth 3 -mindepth 3 -type d)
 for path in ${paths[@]}
 do
@@ -7,3 +23,5 @@ do
     save_path="$base$save"
     dicom2mesh -i $path -t 557 -o $save_path
 done
+echo "program done"
+date
