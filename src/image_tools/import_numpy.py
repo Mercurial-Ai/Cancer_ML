53f5e32ad4ade2c6af124efafb80f243b05465a9
Tristen Pool <tristenpool89@gmail.com>
2022-05-16 09:10:54 -0500

    import numpy 3d fixes

--- src/image_tools/import_numpy.py
+++ src/image_tools/import_numpy.py
@@ -50,24 +50,29 @@ def import_numpy_2d(path, clinical_ids, random_crop=True, crop_size=(512, 512)):
 
 def import_numpy_3d(dir):
     patients = os.listdir(dir)
+    all_vertices = []
     for patient in patients:
         patient = os.path.join(dir, patient)
         patient_mesh = mesh.Mesh.from_file(patient)
         vertices = np.around(np.unique(patient_mesh.vectors.reshape(int(patient_mesh.vectors.size/3), 3), axis=0),2)
-        patient_mesh = np.asarray(patient_mesh)
-        # for instance, if patient mesh shape is (375030, 9),
-        # there are 375030 faces and each face has three vertices with 
-        # three coordinate values each. Therefore, vertice count is equal to:
-        vertice_count = patient_mesh.shape[0]*np.sqrt(patient_mesh.shape[1])
+        vertices = np.expand_dims(vertices, 0)
+        all_vertices.append(vertices)
 
-        tri_obj = Trimesh()
-        patient_mesh_tri = trimesh.load_mesh(patient, enable_post_processing=True, solid=True)
-        faces=patient_mesh_tri.faces
-        print(vertices)
-        print(faces)
-        tri_obj.vertices=vertices
-        tri_obj.faces=faces
-        #grid = creation.voxelize(mesh=tri_obj, pitch=1)
-        #print(grid)
+    vertice_nums=[]
+    for vertice in all_vertices:
+        vertice_num = vertice.shape[1]
+        vertice_nums.append(vertice_num)
+
+    smallest_patient_size = min(vertice_nums)
+
+    i = 0
+    for vertice in all_vertices:
+        vertice = tf.image.random_crop(vertice, (1, smallest_patient_size, 3))
+        all_vertices[i] = np.asarray(vertice)
+        i = i + 1
+
+    vertices = np.concatenate(all_vertices, axis=0)
+
+    return vertices
 
 import_numpy_3d("C:\\Users\\trist\\cs_projects\\Cancer_Project\\Cancer Imagery\\3d_duke")
