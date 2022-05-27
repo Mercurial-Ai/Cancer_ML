cdef40100f25f8c8a9d9ba4495002d26bc021590
Tristen Pool <tristenpool89@gmail.com>
2022-05-27 14:08:54 -0600

    change partition name

--- run_ex.sh
+++ run_ex.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=28
 #SBATCH --nodes=1
-#SBATCH --partition=All
+#SBATCH --partition=mercurial
 #SBATCH -J braggNN 
 #SBATCH --time=12:00:00
 #SBATCH -e ./%J.err
