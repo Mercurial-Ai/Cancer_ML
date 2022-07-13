8953184bd553b8adc09daeff97ef6f7fb853c485
Tristen Pool <tristenpool89@gmail.com>
2022-07-13 06:41:06 -0600

    lower batch size in image-clinical

--- src/image_model.py
+++ src/image_model.py
@@ -170,7 +170,7 @@ class image_model:
 
         config = {
             'epochs':tune.choice([50, 100, 150]),
-            'batch_size':tune.choice([8, 16, 20]),
+            'batch_size':tune.choice([1, 2, 3, 4]),
             'lr':tune.loguniform(1e-4, 1e-1)
         }      
         scheduler = ASHAScheduler(
